c08caa99eb700a83e966974edb801d9eac27d7ae
Uri Herrera <hello@uriherrera.com>
2021-07-26 17:42:28 -0500

    add latest current kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,8 +6,8 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-headers-5.13.4-051304-generic_5.13.4-051304.202107201535_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-headers-5.13.4-051304_5.13.4-051304.202107201535_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-image-unsigned-5.13.4-051304-generic_5.13.4-051304.202107201535_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-modules-5.13.4-051304-generic_5.13.4-051304.202107201535_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.5/amd64/linux-headers-5.13.5-051305-generic_5.13.5-051305.202107251335_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.5/amd64/linux-headers-5.13.5-051305_5.13.5-051305.202107251335_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.5/amd64/linux-image-unsigned-5.13.5-051305-generic_5.13.5-051305.202107251335_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.5/amd64/linux-modules-5.13.5-051305-generic_5.13.5-051305.202107251335_amd64.deb"
 )
