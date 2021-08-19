6cbea0e34cd7a416facd2d53741d4b4e8dba3ac1
Uri Herrera <hello@uriherrera.com>
2021-08-18 21:11:34 -0500

    add latest xanmod kernels

--- travis-build.sh
+++ travis-build.sh
@@ -13,9 +13,9 @@ sudo apt update
 	cd files
 
 	apt download \
-		linux-headers-5.13.7-xanmod1 \
-		linux-image-5.13.7-xanmod1 \
-		linux-headers-5.13.7-xanmod1-cacule \
-		linux-image-5.13.7-xanmod1-cacule
+		linux-headers-5.13.12-xanmod1 \
+		linux-image-5.13.12-xanmod1 \
+		linux-headers-5.13.12-xanmod1-cacule \
+		linux-image-5.13.12-xanmod1-cacule
 )
 
