5969406236fac502a1e8935935c216d7ee402d9b
Uri Herrera <hello@uriherrera.com>
2021-08-18 21:06:38 -0500

    add latest lts 5.4 kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,9 +6,9 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.137/amd64/linux-headers-5.4.137-0504137-generic_5.4.137-0504137.202107310732_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.137/amd64/linux-headers-5.4.137-0504137_5.4.137-0504137.202107310732_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.137/amd64/linux-image-unsigned-5.4.137-0504137-generic_5.4.137-0504137.202107310732_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.137/amd64/linux-modules-5.4.137-0504137-generic_5.4.137-0504137.202107310732_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.142/amd64/linux-headers-5.4.142-0504142-generic_5.4.142-0504142.202108180735_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.142/amd64/linux-headers-5.4.142-0504142_5.4.142-0504142.202108180735_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.142/amd64/linux-image-unsigned-5.4.142-0504142-generic_5.4.142-0504142.202108180735_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.142/amd64/linux-modules-5.4.142-0504142-generic_5.4.142-0504142.202108180735_amd64.deb"
 )
 
