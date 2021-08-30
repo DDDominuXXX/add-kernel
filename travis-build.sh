8d42d9980e50ea73cd974e6ee78e94695221f68d
Uri Herrera <hello@uriherrera.com>
2021-08-30 01:48:25 -0500

    try again

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,7 @@ set -x
 	cd files
 
 	wget -q \
-		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-5.13.0-13.1-liquorix-amd64_5.13-11ubuntu1~focal_amd64.deb "\
+		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-5.13.0-13.1-liquorix-amd64_5.13-11ubuntu1~focal_amd64.deb"\
 		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-liquorix-amd64_5.13-11ubuntu1~focal_amd64.deb" \
 		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-image-5.13.0-13.1-liquorix-amd64_5.13-11ubuntu1~focal_amd64.deb" \
 		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-image-liquorix-amd64_5.13-11ubuntu1~focal_amd64.deb"
