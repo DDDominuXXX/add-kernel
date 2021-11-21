92c21d979a1695a867a956ebc5e162021bb4e64f
Uri Herrera <hello@uriherrera.com>
2021-11-20 19:50:58 -0600

    add latest current kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,8 +6,8 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.15/amd64/linux-headers-5.14.15-051415-generic_5.14.15-051415.202110270548_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.15/amd64/linux-headers-5.14.15-051415_5.14.15-051415.202110270548_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.15/amd64/linux-image-unsigned-5.14.15-051415-generic_5.14.15-051415.202110270548_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.15/amd64/linux-modules-5.14.15-051415-generic_5.14.15-051415.202110270548_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.20/amd64/linux-headers-5.14.20-051420-generic_5.14.20-051420.202111181431_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.20/amd64/linux-headers-5.14.20-051420_5.14.20-051420.202111181431_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.20/amd64/linux-image-unsigned-5.14.20-051420-generic_5.14.20-051420.202111181431_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.20/amd64/linux-modules-5.14.20-051420-generic_5.14.20-051420.202111181431_amd64.deb"
 )
