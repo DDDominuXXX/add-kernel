68bfade0f1ca55f360dcc381adb394fefbb2fb3f
UriHerrera <hello@uriherrera.com>
2021-12-01 23:21:19 -0600

    ...

--- travis-build.sh
+++ travis-build.sh
@@ -9,5 +9,4 @@
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.21/amd64/linux-headers-5.14.21-051421-generic_5.14.21-051421.202111210831_amd64.deb" \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.21/amd64/linux-headers-5.14.21-051421_5.14.21-051421.202111210831_all.deb" \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.21/amd64/linux-image-unsigned-5.14.21-051421-generic_5.14.21-051421.202111210831_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.21/amd64/linux-image-unsigned-5.14.21-051421-lowlatency_5.14.21-051421.202111210831_amd64.deb"
-)
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14.21/amd64/linux-modules-5.14.21-051421-generic_5.14.21-051421.202111210831_amd64.deb"
