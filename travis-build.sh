8011e9a55a47cc3afeffb06a0a1de34dbba17a8e
UriHerrera <hello@uriherrera.com>
2021-12-25 19:55:34 -0600

    add latest 5.4. kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,9 +6,9 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.163/amd64/linux-headers-5.4.163-0504163-generic_5.4.163-0504163.202112010940_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.163/amd64/linux-headers-5.4.163-0504163_5.4.163-0504163.202112010940_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.163/amd64/linux-image-unsigned-5.4.163-0504163-generic_5.4.163-0504163.202112010940_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.163/amd64/linux-modules-5.4.163-0504163-generic_5.4.163-0504163.202112010940_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.168/amd64/linux-headers-5.4.168-0504168-generic_5.4.168-0504168.202112220936_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.168/amd64/linux-headers-5.4.168-0504168_5.4.168-0504168.202112220936_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.168/amd64/linux-image-unsigned-5.4.168-0504168-generic_5.4.168-0504168.202112220936_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.168/amd64/linux-modules-5.4.168-0504168-generic_5.4.168-0504168.202112220936_amd64.deb"
 )
 
