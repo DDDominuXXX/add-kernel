febe272b431bedf385640c9abefe899ec029bdcd
UriHerrera <hello@uriherrera.com>
2022-01-13 18:12:13 -0600

    add latest liquorix kernel

--- travis-build.sh
+++ travis-build.sh
@@ -7,9 +7,9 @@ set -x
 	cd files
 
 	wget -q \
-		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-5.15.0-11.1-liquorix-amd64_5.15-13ubuntu1~focal_amd64.deb"\
-		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-liquorix-amd64_5.15-13ubuntu1~focal_amd64.deb" \
-		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-image-5.15.0-11.1-liquorix-amd64_5.15-13ubuntu1~focal_amd64.deb" \
-		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-image-liquorix-amd64_5.15-13ubuntu1~focal_amd64.deb"
+		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-5.15.0-14.1-liquorix-amd64_5.15-17ubuntu1~focal_amd64.deb"\
+		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-headers-liquorix-amd64_5.15-17ubuntu1~focal_amd64.deb" \
+		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-image-5.15.0-14.1-liquorix-amd64_5.15-17ubuntu1~focal_amd64.deb" \
+		"https://launchpad.net/~damentz/+archive/ubuntu/liquorix/+files/linux-image-liquorix-amd64_5.15-17ubuntu1~focal_amd64.deb"
 )
 
