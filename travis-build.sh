1a489b5a232d67808d917fa86c198d43268c6732
UriHerrera <hello@uriherrera.com>
2022-01-13 18:32:09 -0600

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,7 @@
 
 	wget -q \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-headers-5.4.171-0504171-generic_5.4.171-0504171.202201111849_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-headers-5.4.171-0504171_5.4.171-0504171.202201111849_all.deb	" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-headers-5.4.171-0504171_5.4.171-0504171.202201111849_all.deb" \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-image-unsigned-5.4.171-0504171-generic_5.4.171-0504171.202201111849_amd64.deb" \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-modules-5.4.171-0504171-generic_5.4.171-0504171.202201111849_amd64.deb"
 )
