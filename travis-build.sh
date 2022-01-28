e3cf58ab6d1819edd0fcde5f63fa18da8f0621e4
UriHerrera <hello@uriherrera.com>
2022-01-28 15:27:06 -0600

    add latest lts kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,9 +6,9 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-headers-5.4.171-0504171-generic_5.4.171-0504171.202201111849_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-headers-5.4.171-0504171_5.4.171-0504171.202201111849_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-image-unsigned-5.4.171-0504171-generic_5.4.171-0504171.202201111849_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.171/amd64/linux-modules-5.4.171-0504171-generic_5.4.171-0504171.202201111849_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.17/amd64/linux-headers-5.15.17-051517-generic_5.15.17-051517.202201271135_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.17/amd64/linux-headers-5.15.17-051517_5.15.17-051517.202201271135_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.17/amd64/linux-image-unsigned-5.15.17-051517-generic_5.15.17-051517.202201271135_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.17/amd64/linux-modules-5.15.17-051517-generic_5.15.17-051517.202201271135_amd64.deb"
 )
 
