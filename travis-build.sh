228af25b66e41a092c19e3c80055ee9bdaf45657
UriHerrera <hello@uriherrera.com>
2022-03-02 22:06:05 -0600

    add latest lts kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,9 +6,9 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.19/amd64/linux-headers-5.15.19-051519-generic_5.15.19-051519.202202011232_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.19/amd64/linux-headers-5.15.19-051519_5.15.19-051519.202202011232_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.19/amd64/linux-image-unsigned-5.15.19-051519-generic_5.15.19-051519.202202011232_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.19/amd64/linux-modules-5.15.19-051519-generic_5.15.19-051519.202202011232_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.26/amd64/linux-headers-5.15.26-051526-generic_5.15.26-051526.202203020644_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.26/amd64/linux-headers-5.15.26-051526_5.15.26-051526.202203020644_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.26/amd64/linux-image-unsigned-5.15.26-051526-generic_5.15.26-051526.202203020644_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.26/amd64/linux-modules-5.15.26-051526-generic_5.15.26-051526.202203020644_amd64.deb"
 )
 
