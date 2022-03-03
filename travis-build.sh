3759003f54bcfa998c1e49dfca0fb2a8f0e1c9b7
UriHerrera <hello@uriherrera.com>
2022-03-03 15:42:01 -0600

    add latest libre lts kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,7 @@
 	cd files
 
 	wget -q \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-libre/linux-headers-5.15.19-gnu_5.15.19-gnu-1.0_amd64.deb" \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-libre/linux-image-5.15.19-gnu_5.15.19-gnu-1.0_amd64.deb"
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-libre/linux-headers-5.15.26-gnu_5.15.26-gnu-1.0_amd64.deb" \
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-libre/linux-image-5.15.26-gnu_5.15.26-gnu-1.0_amd64.deb"
 )
 
